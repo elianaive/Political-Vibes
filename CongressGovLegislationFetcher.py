0516b970e5f3d994679f30ccc4f12aa18542f1cb
elianull <elianull1776@gmail.com>
2024-02-20 02:53:29 -0500

    Obtains legislation, limited to first iteration

--- CongressGovLegislationFetcher.py
+++ CongressGovLegislationFetcher.py
@@ -1,37 +1,74 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import threading
 from tqdm import tqdm
 import requests
 from dotenv import load_dotenv
 import os
-import re
+import pandas as pd
+import xml.etree.ElementTree as ET
 
 load_dotenv()
 
 class CongressGovLegislationFetcher:
-    def __init__(self, api_key, congress):
+    def __init__(self, api_key, congress, max=10):
         self.api_key = api_key
         self.congress = congress
-        self.base_url = "https://api.congress.gov/v3/bill"
+        self.base_url = "https://api.congress.gov/v3"
         self.data_lock = threading.Lock()
         self.condition = threading.Condition(self.data_lock)
         self.bills = []
         self.is_fetching = False
         self.page = 0
         self.finished = False
-        self.limit = 10  # Adjust based on API limits
+        self.limit = 250 if ((max > 250) or max == -1) else max
 
     @staticmethod
     def calculate_congress_date_range(congress):
-        """
-        Calculate the start and end dates for the specified Congress.
-        Each Congress lasts for two years, starting on January 3rd of odd-numbered years.
-        """
-        # Congresses start on January 3rd of odd-numbered years and last for two years.
         start_year = 1789 + (int(congress) - 1) * 2
-        end_year = start_year + 2  # The year after the Congress ends
+        end_year = start_year + 2
         from_date = f"{start_year}-01-03T00:00:00Z"
-        to_date = f"{end_year}-01-03T00:00:00Z"  # Include actions up to the end of the Congress period
+        to_date = f"{end_year}-01-03T00:00:00Z"
         return from_date, to_date
+
+    def get_bill_text(self, congress, bill_number, origin):
+        origin_code = 's' if origin == 'S' else 'hr' if origin == 'H' else origin
+        url = f"https://www.congress.gov/{self.congress}/bills/{origin_code}{bill_number}/BILLS-{self.congress}{origin_code}{bill_number}i{origin.lower()}.xml"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            root = ET.fromstring(response.text)
+
+            # Function to recursively extract text from each element
+            def extract_text(element, texts=[]):
+                if element.text and element.text.strip():
+                    texts.append(element.text.strip())
+                for child in element:
+                    extract_text(child, texts)
+                    if child.tail and child.tail.strip():
+                        texts.append(child.tail.strip())
+                return texts
+
+            # Extract text from the root element
+            all_texts = extract_text(root)
+
+            # Join all texts with new lines to preserve text structure
+            raw_text = " ".join(all_texts).replace("text/xml", "")
+            return raw_text
+        else:
+            print(f"Error extracting text from {origin_code}{bill_number} at {url}")
+            return None
+
+    def fetch_bill_text_concurrently(self, bills_data):
+        with ThreadPoolExecutor() as executor:
+            future_to_bill = {executor.submit(self.get_bill_text, self.congress, bill['bill']['number'], bill['bill']['originChamberCode']): bill for bill in bills_data}
+            for future in as_completed(future_to_bill):
+                bill = future_to_bill[future]
+                try:
+                    bill_text = future.result()
+                    bill['text'] = bill_text  # Append the bill text to the bill data
+                except Exception as exc:
+                    print(f'{bill["bill"]["number"]} generated an exception: {exc}')
+                    bill['text'] = None
 
     def fetch_bills(self):
         while not self.finished:
@@ -40,23 +77,23 @@ class CongressGovLegislationFetcher:
                     return
                 self.is_fetching = True
 
-            from_date, to_date = self.calculate_congress_date_range(congress)
+            from_date, to_date = self.calculate_congress_date_range(self.congress)
             offset = self.page * self.limit
-            url = f"{self.base_url}/{self.congress}?fromDateTime={from_date}&toDateTime={to_date}&sort=updateDate+desc&limit={self.limit}&offset={offset}&api_key={self.api_key}"
+            url = f"{self.base_url}/summaries/{self.congress}?fromDateTime={from_date}&toDateTime={to_date}&sort=updateDate+desc&limit={self.limit}&offset={offset}&api_key={self.api_key}"
 
             try:
                 response = requests.get(url, timeout=10)
                 response.raise_for_status()
                 data = response.json()
-                bills_data = data.get('bills', [])
+                bills_data = data.get('summaries', [])
+
+                if bills_data:
+                    self.fetch_bill_text_concurrently(bills_data)
 
                 with self.condition:
-                    if bills_data:
-                        self.bills.extend(bills_data)
-                        self.page += 1  # Prepare for the next page
-                        if len(bills_data) < self.limit:
-                            self.finished = True  # Last page
-                    else:
+                    self.bills.extend(bills_data)
+                    self.page += 1
+                    if len(bills_data) < self.limit:
                         self.finished = True
 
             except requests.exceptions.RequestException as e:
@@ -69,9 +106,6 @@ class CongressGovLegislationFetcher:
                     self.is_fetching = False
                     self.condition.notify_all()
 
-            if self.finished:
-                break
-
     def next_bill(self):
         with self.condition:
             while not self.bills and not self.finished:
@@ -83,48 +117,58 @@ class CongressGovLegislationFetcher:
                 return self.bills.pop(0)
             return None
 
-def run_test(congress, api_key, num_threads=5, num_items=1):
+def run_test(congress, api_key, num_threads=5, num_items=10):
+    fetcher = CongressGovLegislationFetcher(api_key=api_key, congress=congress, max=num_items)
     bills = []
-    fetcher = CongressGovLegislationFetcher(api_key=api_key, congress=congress)
+
+    # Shared lock for thread-safe operations on shared data
+    lock = threading.Lock()
+    # Counter to track the number of fetched bills
+    bills_fetched = 0
 
     def process_bill(progress):
-        while progress.n < progress.total:
+        nonlocal bills_fetched  # Use the nonlocal keyword to modify the outer scope variable
+        while True:
+            # Only perform the check if num_items is not -1
+            if num_items != -1:
+                with lock:
+                    if bills_fetched >= num_items:
+                        break  # Stop if the desired number of bills has been fetched
+
             bill = fetcher.next_bill()
             if bill:
-                progress.update(1)
-                print(f"Fetched bill: S{bill['number']} - {bill['title']} - {bill['url']}")
-                bills.append(bill)
+                should_update_progress = False
+                with lock:  # Ensure thread-safe operations
+                    # If num_items is -1, continue fetching without checking bills_fetched
+                    # Otherwise, check if the limit has not been reached
+                    if num_items == -1 or bills_fetched < num_items:
+                        bills.append(bill)
+                        bills_fetched += 1
+                        should_update_progress = True
+                    else:
+                        break
+                if should_update_progress:
+                    progress.update(1)
             else:
-                # Exit the loop if no more bills to fetch
+                # No more bills to fetch
                 break
 
-    with tqdm(total=num_items, desc="Fetching bills", unit="bill") as progress:
-        threads = [threading.Thread(target=process_bill, args=(progress,), daemon=True) for _ in range(num_threads)]
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
+    # Adjust total for tqdm based on num_items
+    total_progress = num_items if num_items != -1 else None
+    with tqdm(total=total_progress, desc="Fetching bills", unit="bill") as progress:
+        with ThreadPoolExecutor(max_workers=num_threads) as executor:
+            futures = [executor.submit(process_bill, progress) for _ in range(num_threads)]
+            as_completed(futures)
 
-def get_bill_text(congress, bill_number):
-    print("Getting bill text")
-    # Construct the URL for the bill
-    url = f"https://www.congress.gov/{congress}/bills/s{bill_number}/BILLS-{congress}s{bill_number}is.htm"
-    print(url)
-    
-    # Send a request to the URL and get the response
-    response = requests.get(url)
-    
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        return response.text
-    else:
-        # If the request was not successful, return None
-        return None
-
+    print(f"Total bills fetched: {len(bills)}")
+    bills_df = pd.DataFrame(bills)
+    bills_df.to_csv("data/bills.csv", index=False)
+    with open("test.txt", "w", encoding="utf-8") as file:
+        file.write(bills_df.iloc[0]['text'])
 
 if __name__ == "__main__":
     api_key = os.getenv('CONGRESSGOV_API_KEY')
     if not api_key:
         raise EnvironmentError("CONGRESSGOV_API_KEY environment variable not set.")
     congress = "117"
-    run_test(congress, api_key)
+    run_test(congress, api_key, num_items=-1)
